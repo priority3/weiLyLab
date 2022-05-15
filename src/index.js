4937e642b3a4e72abd5a63906ddec40dd4a709f1
liang zhenqi <1724747604@qq.com>
2022-05-15 14:56:12 +0800

    merge: test
    ISSUES CLOSED: #4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import $ from "jquery";
 $(document).ready(function () {
     console.log("Hello World!");
 });
-$(".eav-content").mouseenter(function(){
-    $(this).addClass("eav-content-hover");
-})
