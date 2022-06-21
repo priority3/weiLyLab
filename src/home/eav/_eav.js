8c7da728424978221a1b301b87092fdbecdbcc98
liang zhenqi <1724747604@qq.com>
2022-06-21 16:17:25 +0800

    fix: fix bug
    ISSUES CLOSED: #4
    ISSUES CLOSED: #13

--- src/home/eav/_eav.js
+++ src/home/eav/_eav.js
@@ -1,11 +1,14 @@
-import $ from "jquery";
+import $ from 'jquery'
 
-$(".eav-content").mouseenter(function(){
-    $(this).siblings().addClass("animation-slide-left");
-});
-$(".drop").mouseenter(function(){
-    $(this).children().css("display", "block");
+// 下拉菜单的实现
+$('.drop').mouseenter(function () {
+  $(this).children('ul').stop(true, false).slideDown(400)
 })
-$(".drop").mouseleave(function(){
-    $(this).children("ul").css("display", "none");
+$('.drop').mouseleave(function () {
+  $(this).children('ul').stop(true, false).slideUp(400)
+})
+// ID点击的动画
+$('.eav-name').click(function () {
+  console.log('click') // 测试用
+  $(this).children('span').addClass('animate__bounce')
 })
