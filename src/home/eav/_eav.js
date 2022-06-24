d4ff40f15ae5e0f16ffa4ecf200e5343677c0e80
liang zhenqi <1724747604@qq.com>
2022-06-24 17:06:29 +0800

    feat: 新的下拉菜单；和背景。
    ISSUES CLOSED: #4
    ISSUES CLOSED: #13

--- src/home/eav/_eav.js
+++ src/home/eav/_eav.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-
+import './eav.scss'
 // 下拉菜单的实现
 $('.drop').mouseenter(function () {
   $(this).children('ul').stop(true, false).slideDown(400)
@@ -11,4 +11,8 @@ $('.drop').mouseleave(function () {
 $('.eav-name').click(function () {
   console.log('click') // 测试用
   $(this).children('span').addClass('animate__bounce')
+})
+// 保持下拉菜单父亲选中效果
+$('.drop').mouseenter(function () {
+  $(this).children('span').addClass('keep')
 })
