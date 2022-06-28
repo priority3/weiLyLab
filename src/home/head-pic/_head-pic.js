ed7c4a405532e4fd4617736c153f876f6cb1eb56
liang zhenqi <1724747604@qq.com>
2022-06-28 18:04:14 +0800

    fix: 成功完成图片自适应问题
    ISSUES CLOSED: #4
    ISSUES CLOSED: #13

--- src/home/head-pic/_head-pic.js
+++ src/home/head-pic/_head-pic.js
@@ -1,1 +1,8 @@
 import './head-pic.scss'
+import $ from 'jquery'
+
+$(window).resize(function () {
+  const high = $(window).height()
+  $('.head-container').css('height', high - 150)
+  console.log(1)
+})
