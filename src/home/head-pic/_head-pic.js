5cc8d54ccb49cbf5110dbec6b8ae35144bdc4169
liang zhenqi <1724747604@qq.com>
2022-07-28 16:22:31 +0800

    fix(#34):
    ISSUES CLOSED: #34

--- src/home/head-pic/_head-pic.js
+++ src/home/head-pic/_head-pic.js
@@ -1,13 +1,41 @@
 import './head-pic.scss'
 import $ from 'jquery'
-
+$(window).ready(function() {
+  const high = $(window).height()
+  $('.head-container').css('height', high - 150)
+})
 $(window).resize(function() {
   const high = $(window).height()
   $('.head-container').css('height', high - 150)
-  console.log(1)
 })
 $(document).ready(function() {
   $('.head-arr').click(function() {
     $('html').scrollTop(830)
   })
+})
+$(window).resize(function() {
+  const width = $(window).width()
+  if (width <= 1600) {
+    if (width <= 1440) {
+      $('html').css('font-size', 42)
+    } else $('html').css('font-size', 45)
+    console.log(123)
+  }
+  if (width >= 1600) {
+    $('html').css('font-size', 50)
+    console.log(321)
+  }
+})
+$(window).ready(function() {
+  const width = $(window).width()
+  if (width <= 1600) {
+    if (width <= 1440) {
+      $('html').css('font-size', 42)
+    } else { $('html').css('font-size', 45) }
+    console.log(123)
+  }
+  if (width >= 1600) {
+    $('html').css('font-size', 50)
+    console.log(321)
+  }
 })
